15321a70bb78b8379e519da801c65c2870885d02
Jan Nils Ferner <contact@jnferner.com>
2022-04-09 23:19:53 +0200

    Add startup config

--- minesweeper/src/main.rs
+++ minesweeper/src/main.rs
@@ -1,3 +1,24 @@
+use bevy::prelude::*;
+
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::WorldInspectorPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    app.insert_resource(WindowDescriptor {
+        title: "Minesweeper".to_string(),
+        width: 800.,
+        height: 600.,
+        ..Default::default()
+    })
+    .add_plugins(DefaultPlugins)
+    .add_startup_system(camera_setup);
+    #[cfg(feature = "debug")]
+    app.add_plugin(WorldInspectorPlugin::new());
+    app.run();
+}
+
+fn camera_setup(mut commands: Commands) {
+    // 2D orthographic camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
