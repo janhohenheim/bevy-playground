9eb47c5ea4754c3c364de179cd7d2486ce0a9cd4
Jan Nils Ferner <contact@jnferner.com>
2022-04-10 00:56:07 +0200

    Respect generation options

--- board-plugin/src/lib.rs
+++ board-plugin/src/lib.rs
@@ -3,7 +3,7 @@ pub mod resources;
 
 use bevy::log;
 use bevy::prelude::*;
-use resources::tile_map::TileMap;
+use resources::{tile_map::TileMap, BoardOptions};
 
 pub struct BoardPlugin;
 
@@ -16,9 +16,17 @@ impl Plugin for BoardPlugin {
 
 impl BoardPlugin {
     /// System to generate the complete Board
-    pub(crate) fn create_board() {
-        let mut tile_map = TileMap::empty(20, 20);
-        tile_map.place_mines(40);
+    pub(crate) fn create_board(
+        mut commands: Commands,
+        board_options: Option<Res<BoardOptions>>,
+        window: Option<Res<WindowDescriptor>>,
+    ) {
+        let options = match board_options {
+            None => Default::default(),
+            Some(options) => options.clone(),
+        };
+        let mut tile_map = TileMap::empty(options.map_size.0, options.map_size.1);
+        tile_map.place_mines(options.mine_count);
         #[cfg(feature = "debug")]
         log::info!("{}", tile_map.console_output());
     }
