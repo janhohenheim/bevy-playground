49ab315bd921b32cb7a20f27681036c61529e784
Jan Nils Ferner <contact@jnferner.com>
2022-04-10 01:27:53 +0200

    Draw sprites for board and tiles

--- board-plugin/src/lib.rs
+++ board-plugin/src/lib.rs
@@ -1,9 +1,10 @@
 pub mod components;
 pub mod resources;
 
+use crate::components::Coordinates;
 use bevy::log;
 use bevy::prelude::*;
-use resources::{tile_map::TileMap, BoardOptions};
+use resources::{tile_map::TileMap, BoardOptions, BoardPosition, TileSize};
 
 pub struct BoardPlugin;
 
@@ -19,7 +20,7 @@ impl BoardPlugin {
     pub(crate) fn create_board(
         mut commands: Commands,
         board_options: Option<Res<BoardOptions>>,
-        window: Option<Res<WindowDescriptor>>,
+        windows: Res<Windows>,
     ) {
         let options = match board_options {
             None => Default::default(),
@@ -29,5 +30,88 @@ impl BoardPlugin {
         tile_map.place_mines(options.mine_count);
         #[cfg(feature = "debug")]
         log::info!("{}", tile_map.console_output());
+
+        let tile_size = match options.tile_size {
+            TileSize::Fixed(size) => size,
+            TileSize::Adaptive { min, max } => Self::calculate_adaptative_tile_size(
+                windows,
+                (min, max),
+                (tile_map.width, tile_map.height),
+            ),
+        };
+
+        let board_size = Vec2::new(
+            tile_map.width as f32 * tile_size,
+            tile_map.height as f32 * tile_size,
+        );
+        log::info!("Board size: {}", board_size);
+
+        let board_position = match options.position {
+            BoardPosition::Centered { offset } => {
+                // Using the bottom left as anchor position
+                Vec3::new(-(board_size.x / 2.), -(board_size.y / 2.), 0.) + offset
+            }
+            BoardPosition::Custom(position) => position,
+        };
+
+        commands
+            .spawn()
+            .insert(Name::new("Board"))
+            .insert(Transform::from_translation(board_position))
+            .insert(GlobalTransform::default())
+            .with_children(|parent| {
+                // We spawn the board background sprite at the center of the board, since the sprite pivot is centered
+                parent
+                    .spawn_bundle(SpriteBundle {
+                        sprite: Sprite {
+                            color: Color::WHITE,
+                            custom_size: Some(board_size),
+                            ..Default::default()
+                        },
+                        transform: Transform::from_xyz(board_size.x / 2., board_size.y / 2., 0.),
+                        ..Default::default()
+                    })
+                    .insert(Name::new("Board Background"));
+
+                for (y, line) in tile_map.iter().enumerate() {
+                    for (x, _) in line.iter().enumerate() {
+                        parent
+                            .spawn_bundle(SpriteBundle {
+                                sprite: Sprite {
+                                    color: Color::GRAY,
+                                    custom_size: Some(Vec2::splat(
+                                        tile_size - options.tile_padding as f32,
+                                    )),
+                                    ..Default::default()
+                                },
+                                transform: Transform::from_xyz(
+                                    (x as f32 * tile_size) + (tile_size / 2.),
+                                    (y as f32 * tile_size) + (tile_size / 2.),
+                                    // Closer to camera -> Drawn over background
+                                    1.,
+                                ),
+                                ..Default::default()
+                            })
+                            .insert(Name::new(format!("Tile ({}, {})", x, y)))
+                            .insert(Coordinates {
+                                x: x as u16,
+                                y: y as u16,
+                            });
+                    }
+                }
+            });
+    }
+
+    fn calculate_adaptative_tile_size(
+        windows: Res<Windows>,
+        (min, max): (f32, f32),
+        (width, heigh): (u16, u16),
+    ) -> f32 {
+        let window = windows
+            .get_primary()
+            .expect("Error: No primary window found when generating board");
+        let max_width = window.width() / width as f32;
+        let max_height = window.height() / heigh as f32;
+        max_width.min(max_height).clamp(min, max)
     }
 }
